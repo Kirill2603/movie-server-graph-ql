ba4b1d4d44377e789d042351138ed7e8d123e12f
Kirill <64753246+Kirill2603@users.noreply.github.com>
2022-03-31 23:17:18 +0300

    fix app

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const graphqlHTTP = require('express-graphql').graphqlHTTP
 const schema = require('../schema/schema')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 const app = express()
 const PORT = 3005
@@ -14,6 +15,8 @@ mongoose.connect(
         useUnifiedTopology: true,
     }
 )
+
+app.use(cors())
 
 app.use('/graphql', graphqlHTTP({
     schema,
